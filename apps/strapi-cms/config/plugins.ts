877300d9bd9a50419c5481a7af7923cb96a64e1a
Ankit Gaurav <ankitgv2017@gmail.com>
2025-01-05 23:25:52 +0530

    Some configurations

--- apps/strapi-cms/config/plugins.ts
+++ apps/strapi-cms/config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+        region: "ap-south-1",
+        params: {
+          Bucket: env("S3_BUCKET_NAME"),
+        },
+      },
+    },
+  });
+  
