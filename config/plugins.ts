57b7dd6ac6b53adefeb4392d6c47c23f0069201e
Ankit Gaurav <ankitgv2017@gmail.com>
2025-01-07 00:24:07 +0530

    Last Try

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,15 @@
 export default ({ env }) => ({
-    upload: {
-      provider: "aws-s3",
+  upload: {
+    config: {
+      provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
-        region: "ap-south-1",
-        params: {
-          Bucket: env("S3_BUCKET_NAME"),
+        s3Options: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+          region: 'ap-south-1',
+          params: {
+            Bucket: env('S3_BUCKET_NAME'),
+          },
         },
       },
       actionOptions: {
@@ -15,5 +18,5 @@ export default ({ env }) => ({
         delete: {},
       },
     },
-  });
-  
+  },
+});
